a642068e47a9cb8b701422afed910cd03c55d990
urban6622 <urbanuss6622@mail.ru>
2024-01-03 00:08:18 +0300

    атак

---
+++ Assets/Source/Scripts/ECS/Components/HealthData.cs
@@ -0,0 +1,30 @@
+﻿namespace Source.Scripts.ECS.Components
+{
+    public struct HealthData
+    {
+        private float _maxValue;
+        private float _currentValue;
+        
+        public float MaxValue
+        {
+            get => _maxValue;
+            set
+            {
+                _maxValue = value;
+                if (_currentValue > _maxValue) _currentValue = _maxValue;
+            }
+        }
+
+        public float CurrentValue
+        {
+            get => _currentValue;
+            set => _currentValue = value > _maxValue ? _maxValue : value;
+        }
+
+        public void InitializeValues(float value)
+        {
+            _maxValue = value;
+            _currentValue = value;
+        }
+    }
+}
---
+++ Assets/Source/Scripts/ECS/Marks/DeadMark.cs
@@ -0,0 +1,8 @@
+﻿namespace Source.Scripts.ECS.Marks
+{
+    // сущность мертва
+    public struct DeadMark
+    {
+        
+    }
+}
